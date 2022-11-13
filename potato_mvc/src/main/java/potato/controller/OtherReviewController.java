bd65bb2485248f9f8c50e8d3c212f404e62ad743
user <user@DESKTOP-COJ9858>
2022-11-13 18:56:23 +0900

    yulfile

--- potato_mvc/src/main/java/potato/controller/OtherReviewController.java
+++ potato_mvc/src/main/java/potato/controller/OtherReviewController.java
@@ -1,20 +1,26 @@
 package potato.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
 @Controller
 public class OtherReviewController {
 	
 	//Å¸»ç¿ëÀÚ¸®ºä
-	@RequestMapping(value="/OtherReviewMove.do", method=POST)
+	@RequestMapping(value="/otherReviewMove.do", method=GET)
 	public String OtherReviewMove(HttpSession session, String id, Model model) {
 		return "other_profiles/jsp/other_user_profiles";
 	}//OtherReviewMove
+	
+	//½Å°í)¸®ºä-ÆË¾÷
+	@RequestMapping(value = "/reportReview.do",method = GET)
+	public String reportReview(HttpSession session, String string) {
+		return "mypages/jsp/report_review_popup";
+	}//reportReview
 
 }
