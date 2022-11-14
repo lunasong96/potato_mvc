d0609b26028d8aba789dd4fe32e53847ec6b56f9
user <user@DESKTOP-COJ9858>
2022-11-15 00:01:40 +0900

    다오 서비스 완료!

--- potato_mvc/src/main/java/potato/dao/MyPageDAO.java
+++ potato_mvc/src/main/java/potato/dao/MyPageDAO.java
@@ -1,4 +1,100 @@
 package potato.dao;
 
+import java.util.List;
+
+import potato.domain.MyPageMyInfoDomain;
+import potato.domain.MyPageReportDomain;
+import potato.manager.domain.ReviewDomain;
+import potato.vo.MyPageLikeVO;
+import potato.vo.MyPageMyInfoEditVO;
+import potato.vo.MyPageMyReviewVO;
+import potato.vo.MyPagePwEditVO;
+import potato.vo.MyPageQuitVO;
+import potato.vo.MypageLikeReviewVO;
+import potato.vo.MypageReportVO;
+
 public class MyPageDAO {
+	
+	//마이페이지 접속
+	public String selectLogin(String string) {
+		return null;
+	}//selectLogin
+	
+	//내 정보 조회
+	public MyPageMyInfoDomain selectInfo(MyPageMyInfoEditVO mieVO) {
+		return null;
+	}//selectInfo
+	
+	//내 정보 수정
+	public int updateInfo(MyPageMyInfoEditVO mieVO) {
+		return 0;
+	}//updateInfo
+	
+	//비밀번호 수정
+	public int updatePw(MyPagePwEditVO peVO) {
+		return 0;
+	}//updatePw
+	
+	//회원 탈퇴
+	public int updateQuit(MyPageQuitVO qVO) {
+		return 0;
+	}//updateQuit
+	
+	//리뷰 조회
+	public List<ReviewDomain> selectMyReview(String id){
+		return null;
+	}//selectMyReview
+	
+	//총 리뷰 수
+	public int selectMyReviewCnt(String string) {
+		return 0;
+	}//selectMyReviewCnt
+	
+	//내 리뷰 - 좋아요 수 
+	public int selectLikeCnt(String string) {
+		return 0;
+	}//selectLikeCnt
+	
+	//내 리뷰 - 리뷰 삭제, 수정
+	public int updateMyReview(MyPageMyReviewVO mrVO) {
+		return 0;
+	}//updateMyReview
+	
+	//좋아요 한 리뷰 조회
+	public List<ReviewDomain>selectLikeReview(MypageLikeReviewVO lrVO){
+		return null;
+	}//selectLikeReview
+	
+	//좋아요 한 리뷰 수
+	public int selectLikeReviewCnt(MypageLikeReviewVO lrVO) {
+		return 0;
+	}//selectLikeReviewCnt
+	
+	//좋아요한 리뷰 :좋아요 수 조회
+	public int selectLikeCnt(MypageLikeReviewVO lrVO) {
+		return 0;
+	}//selectLikeCnt
+	
+	//좋아요한 리뷰 : 좋아요 추가
+	public void insertLike(MyPageLikeVO lVO) {
+		
+	}//insertLike
+	
+	//좋아요한 리뷰 : 좋아요 취소
+	public int delLike(MyPageLikeVO lVO) {
+		return 0;
+	}//delLike
+	
+	//좋아요한 리뷰 - 신고창 팝업
+	public MyPageReportDomain selectReportPopup() {
+		return null;
+	}//selectReportPopup
+	
+	//좋아요한 리뷰 - 신고하기
+	public void insertReviewReport(MypageReportVO rVO) {
+		
+	}//insertReviewReport
+	
+	
+	
 }//class
--- potato_mvc/src/main/java/potato/service/MyPageService.java
+++ potato_mvc/src/main/java/potato/service/MyPageService.java
@@ -1,6 +1,20 @@
 package potato.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import potato.domain.MyPageBookmarkDomain;
+import potato.domain.MyPageReportDomain;
+import potato.domain.MyPageReviewDomain;
+import potato.manager.domain.ReviewDomain;
 import potato.vo.LoginVO;
+import potato.vo.MyPageLikeVO;
+import potato.vo.MyPageMyInfoEditVO;
+import potato.vo.MyPageMyReviewVO;
+import potato.vo.MyPagePwEditVO;
+import potato.vo.MyPageQuitVO;
+import potato.vo.MypageReportVO;
 
 public class MyPageService {
 	
@@ -23,5 +37,100 @@ public class MyPageService {
 	}//searchMember
 	
 	//내 정보 수정
-	//public int 
+	public int updateInfo(MyPageMyInfoEditVO mieVO) {
+		
+		return 0; 
+	}//updateInfo
+	
+	//비밀번호 수정 처리
+	public int modifyPw(MyPagePwEditVO peVO) {
+		return 0;
+	}//modifyPw
+	
+	//회원탈퇴
+	public int updateQuit(MyPageQuitVO qVO) {
+		return 0;
+	}//updateQuit
+	
+	//회원탈퇴 처리
+	public String quitProcess(HttpSession session, MyPageQuitVO qVO) {
+		return null;
+	}//quitProcess
+	
+	//리뷰조회 
+	public String searchMyReview(List<MyPageReviewDomain> list) {
+		return null;
+	}//searchMyReview
+	
+	//총 리뷰 수
+	public int searchMyReviewCnt(String id) {
+		return 0;
+	}//searchMyReviewCnt
+	
+	//좋아요 수
+	public int searchLikeCnt(MyPageLikeVO lVO) {
+		return 0;
+	}//searchLikeCnt
+	
+	//내 리뷰 삭제, 수정
+	public int updateMyReview(MyPageMyReviewVO mrVO) {
+		return 0;
+	}//updateMyReview
+	
+	//페이징(도려옴 ㅎㅎ)
+	//내가 원하는 보여줄 페이지 수를 먼저 넣고 만약에 초과할 경우 isLast를 매개변수로 새로 set
+		public int isLast(int lastPage, int startNum) {
+			
+			return 0;
+		}
+	public int searchLikeReview(MyPageMyReviewVO rVO) {
+		return 0;
+	}//searchLikeReview
+		
+	//좋아요한 리뷰 조회
+	public List<ReviewDomain> searchLikeReview(String id){
+		return null;
+	}//searchLikeReview
+	
+	//리뷰 수
+	public int searchLikeReviewCnt(String id) {
+		return 0;
+	}//searchLikeReviewCnt
+	
+	//좋아요 수 조회
+	public int searchLikeCnt(String id) {
+		return 0;
+	}//searchLikeCnt
+	
+	//좋아요 추가
+	public void addLike(MyPageLikeVO lVO) {
+		
+	}//addLike
+	
+	//좋아요 취소
+	public int delLike(MyPageLikeVO lVO) {
+		return 0;
+	}//delLike
+	
+	//신고창 팝업(이거 맞나,,,)
+	public MyPageReportDomain reportPopup() {
+		return null;
+	}//MyPageReportDomain
+	
+	//신고하기(처리과정)
+	public String reportReview(MypageReportVO rVO) {
+		return null;
+	}//reportReview
+	
+	//북마크한 휴게소 조회
+	public List<MyPageBookmarkDomain>searchBookmark(String string){
+		return null;
+	}//searchBookmark
+	
+	//북마크한 휴게소 삭제(아이디, 휴게소 인덱스-int)
+	public int removeBookmark(String string, int restarea_idx) {
+		return 0;
+	}//removeBookmark
+	
+	
 }//class
