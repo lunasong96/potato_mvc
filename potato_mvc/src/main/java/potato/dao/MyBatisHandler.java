07e09622692cf0967c1475156cb4230c12437c05
user <user@DESKTOP-COJ9858>
2022-11-14 23:40:13 +0900

    유저 로그인 회원가입 관련 DAO 업데이트+Handler

---
+++ potato_mvc/src/main/java/potato/dao/MyBatisHandler.java
@@ -0,0 +1,70 @@
+package potato.dao;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ *	MyBatis Handler를 반환하는 일. ( singleton pattern)
+ * @author user
+ */
+public class MyBatisHandler {
+	
+	private static MyBatisHandler mbh;
+	private static SqlSessionFactory ssf;
+	
+	private MyBatisHandler() {
+		org.apache.ibatis.logging.LogFactory.useLog4JLogging();
+	}
+	
+	/**
+	 * factory-method="getInstance"
+	 * @return
+	 */
+	public static MyBatisHandler getInstance() {
+		if(mbh==null) {
+			mbh=new MyBatisHandler();
+		}//end if
+		return mbh;
+	}//getInstance()
+	
+	private static SqlSessionFactory getSessionFactory() throws IOException {
+		if( ssf == null ) {
+			//1. 설정파일 연결
+			String configPath="potato/dao/config/mybatis-config.xml";
+			Reader reader=Resources.getResourceAsReader(configPath);
+			//2. MyBatis Framework 생성
+			//3. MyBatis Framework 할당
+			ssf=new SqlSessionFactoryBuilder().build(reader);
+			//3. 스트림 닫기
+			if(reader != null) {
+				reader.close();
+			}//end if
+		}//end if
+		return ssf;
+	}//getSessionFactory
+	
+	/**
+	 * MyBatis Handler을 반환
+	 * @return
+	 */
+	public SqlSession getHandler() {
+		SqlSession session = null;
+		
+		try {
+			session = getSessionFactory().openSession();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}//end catch
+		return session;
+	}//getMyBatisHandler
+	
+	public void closeHandler(SqlSession session) {
+		session.close();
+	}//closeHandler
+	
+}
---
+++ potato_mvc/src/main/java/potato/dao/UserDAO.java
@@ -0,0 +1,150 @@
+package potato.dao;
+
+import potato.vo.ForgotIdVO;
+import potato.vo.ForgotPwVO;
+import potato.vo.LoginVO;
+import potato.vo.ManagerLoginVO;
+import potato.vo.UserInfoVO;
+
+import org.apache.ibatis.session.SqlSession;
+
+import potato.dao.MyBatisHandler;
+/**
+ *	유저의 로그인, 회원가입 관련 DAO
+ * @author user
+ */
+public class UserDAO {
+	//mapper namespace 변수 선언
+	public static final String UserMapper="potato.UserMapper.";
+	
+	/**
+	 * 유저 회원가입
+	 * @param uiVO
+	 */
+	public int insertMember(UserInfoVO uiVO) {
+		int cnt = 0;
+		
+		//1. Handler 얻기
+		MyBatisHandler mbh= MyBatisHandler.getInstance();
+		SqlSession ss= mbh.getHandler();
+		//2. 쿼리실행
+		cnt=ss.insert(UserMapper+"insertMember", uiVO);
+		//3. 끊기
+		mbh.closeHandler(ss);
+		
+		return cnt;
+	}//insertMember
+	
+	/**
+	 * 유저 로그인
+	 * @param lVO
+	 * @return
+	 */
+	public String selectLogin(LoginVO lVO) {
+		String result;
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss=mbh.getHandler();
+		result =ss.selectOne(UserMapper+"selectMemberLogin", lVO);
+		mbh.closeHandler(ss);
+		return result;
+	}//selectLogin
+	
+	/**
+	 * 매니저 로그인
+	 * @param mlVO
+	 * @return
+	 */
+	public String selectManager(ManagerLoginVO mlVO) {
+		String result;
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss=mbh.getHandler();
+		result = ss.selectOne(UserMapper+"selectManagerLogin", mlVO);
+		mbh.closeHandler(ss);
+		return result;
+	}//selectManager
+	
+	/**
+	 * 아이디 중복확인
+	 * @param id
+	 * @return 
+	 */
+	public String selectMDuplChkId(String id) {
+		String result = "";
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss= mbh.getHandler();
+		
+		result = ss.selectOne(UserMapper+"selectDuplChkId", id);
+		mbh.closeHandler(ss);
+		return result;
+	}//selectMDuplChkId
+	
+	/**
+	 * 닉네임 중복 검사
+	 * @param nick
+	 * @return 
+	 */
+	public String selectMDuplChkNick(String nick) {
+		String result ="";
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss= mbh.getHandler();
+		
+		result = ss.selectOne(UserMapper+"selectDuplChkNick", nick);
+		mbh.closeHandler(ss);
+		return result;
+	}//selectMDuplChkNick
+	
+	/**
+	 * 아이디 찾기
+	 * @param fiVO
+	 * @return
+	 */
+	public String selectId(ForgotIdVO fiVO) {
+		String id="";
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss= mbh.getHandler();
+		
+		id= ss.selectOne(UserMapper+"selectForgotId", fiVO);
+		mbh.closeHandler(ss);
+		
+		return id;
+	}//selectId
+	
+	/**
+	 * 비밀번호 찾기
+	 * @param fpVO
+	 * @return
+	 */
+	public String selectPw(ForgotPwVO fpVO) {
+		String pw="";
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss= mbh.getHandler();
+		
+		pw= ss.selectOne(UserMapper+"selectForgotPw", fpVO);
+		mbh.closeHandler(ss);
+		
+		return pw;
+	}//selectId
+	
+	/**
+	 * 임시비밀번호 업데이트
+	 * @param fpVO
+	 * @return
+	 */
+	public int updatePw(ForgotPwVO fpVO) {
+		int cnt=0;
+		
+		MyBatisHandler mbh=MyBatisHandler.getInstance();
+		SqlSession ss= mbh.getHandler();
+		cnt = ss.update(UserMapper, fpVO);
+		mbh.closeHandler(ss);
+		return cnt;
+	}//updatePw
+	
+	
+}//class
